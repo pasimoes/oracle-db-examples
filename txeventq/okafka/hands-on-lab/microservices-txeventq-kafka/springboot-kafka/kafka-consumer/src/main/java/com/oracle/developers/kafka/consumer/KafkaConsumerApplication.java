198bbfbee6742b7172941a14bff300aaed24e085
Paulo Alberto Simoes <paulo.simoes@oracle.com>
2024-08-19 18:23:30 -0300

    Issue #381: WIP OKafka Hands-on-lab
    Signed-off-by: Paulo Alberto Simoes <paulo.simoes@oracle.com>

---
+++ txeventq/okafka/hands-on-lab/microservices-txeventq-kafka/springboot-kafka/kafka-consumer/src/main/java/com/oracle/developers/kafka/consumer/KafkaConsumerApplication.java
@@ -0,0 +1,35 @@
+package com.oracle.developers.kafka.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.kafka.core.KafkaOperations;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.util.backoff.FixedBackOff;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.oracle.developers.kafka",
+        excludeFilters =
+        @ComponentScan.Filter(type= FilterType.REGEX,
+                pattern="com\\.oracle\\.developers\\.kafka\\.config\\.producer\\..*"))
+public class KafkaConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaConsumerApplication.class, args);
+    }
+
+
+    /*
+     * Boot will autowire this into the container factory.
+     */
+    @Bean
+    public DefaultErrorHandler errorHandler(KafkaOperations<Object, Object> template) {
+        return new DefaultErrorHandler(
+                new DeadLetterPublishingRecoverer(template), new FixedBackOff(1000L, 2));
+    }
+
+
+}
